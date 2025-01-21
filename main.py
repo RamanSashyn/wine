807633e7d2b448dc0f7dcf1bc24de615756fe105
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-21 16:54:06 +0100

    Интегрирование скриптов для возраста фирмы и правильной формы слова

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from company_age_calculator import calculate_company_age
+from word_form_generator import get_year_word_form
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    store_age=calculate_company_age(),
+    word_form=get_year_word_form(),
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
