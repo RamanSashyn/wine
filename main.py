f4ef784d41367881b7a165e5f5633238f1c68f4a
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-22 14:46:25 +0100

    Добавление вин с использованием ексель файла

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from company_age_calculator import calculate_company_age
 from word_form_generator import get_year_word_form
+from product_data_reader import read_product_data
 
 
 env = Environment(
@@ -13,9 +14,12 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+wine_data = read_product_data()
+
 rendered_page = template.render(
     store_age=calculate_company_age(),
     word_form=get_year_word_form(),
+    wines=wine_data,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
