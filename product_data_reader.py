6e2c85ece56577c5a0a4002bf499c9aabbdbd697
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-22 15:40:33 +0100

    Улучшение структуры кода

--- product_data_reader.py
+++ product_data_reader.py
@@ -1,22 +1,13 @@
 import pandas
-from pprint import pprint
+from collections import defaultdict
 
-
-def read_product_data():
-    excel_data_df = pandas.read_excel("wine.xlsx", sheet_name="Лист1")
-    product_data = excel_data_df.to_dict(orient="records")
-    return product_data
 
 def read_new_product_data():
-    excel_data_df = pandas.read_excel("wine2.xlsx", sheet_name="Лист1" )
+    excel_data_df = pandas.read_excel("wine3.xlsx", sheet_name="Лист1" )
     excel_data_df = excel_data_df.fillna("")
 
-    grouped_products = {}
-    for category, group in excel_data_df.groupby("Категория"):
-        grouped_products[category] = group.to_dict(orient="records")
+    grouped_products = defaultdict(list)
+    for _, row in excel_data_df.iterrows():
+        grouped_products[row["Категория"]].append(row.to_dict())
 
     return grouped_products
-
-if __name__ == "__main__":
-    product_data = read_new_product_data()
-    pprint(product_data)
